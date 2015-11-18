5f06d82c7296ef694ecf2f98bade312a25658e4b
Hector Castro <hectcastro@gmail.com>
2015-11-17 22:42:59 -0500

    Apply hclfmt
    See: https://github.com/fatih/hclfmt

--- variables.tf
+++ variables.tf
@@ -1,18 +1,27 @@
-variable "vpc_id" { }
-variable "vpc_cidr_block" { }
+variable "vpc_id" {
+}
 
-variable "cache_name" { }
+variable "vpc_cidr_block" {
+}
+
+variable "cache_name" {
+}
+
 variable "engine_version" {
   default = "2.8.22"
 }
+
 variable "instance_type" {
   default = "cache.t2.micro"
 }
+
 variable "maintenance_window" {
   # SUN 01:00AM-02:00AM ET
   default = "sun:05:00-sun:06:00"
 }
 
-variable "private_subnet_ids" { }
+variable "private_subnet_ids" {
+}
 
-variable "alarm_actions" { }
+variable "alarm_actions" {
+}
