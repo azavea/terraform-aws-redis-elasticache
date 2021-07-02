b42d373706d529e0d8064f9c661416c96c569256
rajaskumar <74214534+rajaskumar@users.noreply.github.com>
2021-07-02 00:07:05 -0400

    Update main.tf
    Modified alarm name and id based on project name .${var.project}

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ resource "aws_elasticache_replication_group" "redis" {
 resource "aws_cloudwatch_metric_alarm" "cache_cpu" {
   count = "${var.desired_clusters}"
 
-  alarm_name          = "alarm${var.environment}CacheCluster00${count.index + 1}CPUUtilization"
+  alarm_name          = "alarm${var.project}${var.environment}CacheCluster00${count.index + 1}CPUUtilization"
   alarm_description   = "Redis cluster CPU utilization"
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods  = "1"
@@ -62,7 +62,7 @@ resource "aws_cloudwatch_metric_alarm" "cache_cpu" {
 resource "aws_cloudwatch_metric_alarm" "cache_memory" {
   count = "${var.desired_clusters}"
 
-  alarm_name          = "alarm${var.environment}CacheCluster00${count.index + 1}FreeableMemory"
+  alarm_name          = "alarm${var.project}${var.environment}CacheCluster00${count.index + 1}FreeableMemory"
   alarm_description   = "Redis cluster freeable memory"
   comparison_operator = "LessThanThreshold"
   evaluation_periods  = "1"
